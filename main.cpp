c46041a3fb68418b69ab3fe2bb8145200b3a2ee8
Crazyteea <lipatow.nikita@yandex.ru>
2019-12-19 22:13:48 +0300

    imgui

--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "hazedumper/csgo.hpp"
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << hazedumper::signatures::dwLocalPlayer;
-    return 0;
-}
